5bdc14465b7258cb892c4bdb8a6e1f3cff8e3a40
ashish28-art <ashishsai0628@gmail.com>
2026-01-18 16:36:55 +0530

    fix build:remove firebase error

--- vite.config.js
+++ vite.config.js
@@ -4,20 +4,21 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:process.env.VITE_BASE_PATH ||"/Vervana",
+  base: process.env.VITE_BASE_PATH || "/Vervana",
   build: {
     rollupOptions: {
       output: {
         manualChunks: {
           'react-vendor': ['react', 'react-dom', 'react-router-dom'],
-          'firebase-vendor': ['firebase'],
           'ui-vendor': ['@mui/material', '@emotion/react', '@emotion/styled', 'framer-motion'],
         },
+
       },
     },
     chunkSizeWarningLimit: 1000,
   },
   optimizeDeps: {
     include: ['react', 'react-dom', 'react-router-dom'],
+    exclude:['firebase']
   },
 })
